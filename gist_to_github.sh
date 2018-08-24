50663d82a095388265c71052e4d31854e2784bc8
Pacôme Françon <pacome.francon@epitech.eu>
2018-08-24 09:53:40 +0200

    create subfolder with source file within it

---
+++ gist_to_github.sh
@@ -0,0 +1,27 @@
+folder=my_gists
+mkdir $folder 2>/dev/null
+cd $folder
+
+data=`curl -s 'https://api.github.com/users/pacome35220/gists'`
+
+urls=$(echo $data | jq -r .[].files | jq -r .[].raw_url)
+descriptions=$(echo $data | jq -r .[].description)
+nb_comments=$(echo $data | jq -r .[].comments)
+
+for url in $urls
+do
+    tmp=$IFS
+    IFS=''
+
+    gist=`curl -s $url`
+
+    filename=$(echo $url | cut -f8 -d /)
+
+    subfolder=${filename::-2}
+    mkdir $subfolder 2>/dev/null
+
+    echo $gist > $subfolder/$filename
+
+    IFS=$tmp
+    echo -e "\e[033m" $filename : "\e[32m" DONE "\n"
+done
