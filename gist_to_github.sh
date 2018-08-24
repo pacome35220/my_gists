daa7af5e40b8cd141c76c9dbe8be66946ac3a60b
rudreais <valentinlattay@gmail.com>
2018-08-24 18:09:28 +0200

    fixed array

--- gist_to_github.sh
+++ gist_to_github.sh
@@ -1,14 +1,19 @@
+#!/bin/bash
+OLDIFS="$IFS" # keep a backup here
+
 data="$(curl -s 'https://api.github.com/users/pacome35220/gists')"
 
+IFS=$' ' # parsing the string
 urls=$(echo $data | jq -r .[].files | jq -r .[].raw_url)
+IFS=$'\n' # parsing the array
+urls_arr=($urls)
+
 descriptions=$(echo $data | jq -r .[].description)
 nb_comments=$(echo $data | jq -r .[].comments)
 
-for url in $urls
+for url in "${urls_arr}"
 do
-    tmp=$IFS
-    IFS=''
-
+	echo $url
     gist="$(curl -s $url)"
 
     filename=$(echo $url | cut -f8 -d /)
