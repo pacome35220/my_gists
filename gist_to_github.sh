cf3989550e8bbf26f4152736409510ea93388db4
rudreais <valentin.lattay@epitech.eu>
2018-08-24 18:16:37 +0200

    added quotes

--- gist_to_github.sh
+++ gist_to_github.sh
@@ -4,25 +4,25 @@ OLDIFS="$IFS" # keep a backup here
 data="$(curl -s 'https://api.github.com/users/pacome35220/gists')"
 
 IFS=$' ' # parsing the string
-urls=$(echo $data | jq -r .[].files | jq -r .[].raw_url)
+urls="$(echo $data | jq -r .[].files | jq -r .[].raw_url)"
 IFS=$'\n' # parsing the array
 urls_arr=($urls)
 
-descriptions=$(echo $data | jq -r .[].description)
-nb_comments=$(echo $data | jq -r .[].comments)
+descriptions="$(echo $data | jq -r .[].description)"
+nb_comments="$(echo $data | jq -r .[].comments)"
 
 for url in "${urls_arr[@]}"
 do
 	echo $url
     gist="$(curl -s $url)"
 
-    filename=$(echo $url | cut -f8 -d /)
+    filename="$(echo $url | cut -f8 -d /)"
 
-    subfolder=${filename::-2}
-    mkdir $subfolder 2>/dev/null
+    subfolder="${filename::-2}"
+    mkdir "$subfolder" 2>/dev/null
 
-    echo $gist > $subfolder/$filename
+    echo "$gist" > "$subfolder"/"$filename"
 
-    IFS=$tmp
-    echo -e "\e[033m" $filename : "\e[32m" DONE "\e[0m\n"
+    echo -e "\e[033m" "$filename" : "\e[32m" DONE "\e[0m\n"
 done
+IFS="$OLDIFS"
