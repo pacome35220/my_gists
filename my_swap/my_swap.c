bf4bfe608f75e03174fa96c1c99082c95592e7cf
ReimuA <thibault.dolley@epitech.eu>
2018-10-12 14:07:56 +0200

    added generic macro

--- my_swap/my_swap.c
+++ my_swap/my_swap.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
+#include <stdint.h>
+
+// This should work on all primitive besides float.
+
+#define swap(type, x, y) do {			\
+    x = (type)((uintptr_t)x ^ (uintptr_t)y);	\
+    y = (type)((uintptr_t)y ^ (uintptr_t)x);	\
+    x = (type)((uintptr_t)x ^ (uintptr_t)y);	\
+  } while (0)
 
 int main()
 {
-  int foo = 10;
-  int bar = 9321;
+  char *foo = "10";
+  char *bar = "-932";
 
-  printf("foo: %d, bar: %d\n", foo, bar);
-  foo ^= bar;
-  bar ^= foo;
-  foo ^= bar;
-  printf("foo: %d, bar: %d\n", foo, bar);
+  printf("foo: %s, bar: %s\n", foo, bar);
+  swap(char *, foo, bar);
+  printf("foo: %s, bar: %s\n", foo, bar);
 }
