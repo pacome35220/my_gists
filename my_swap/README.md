423fdeb28397f09be46f5722365633ac88cc6d81
Pacôme Françon <pacome.francon@epitech.eu>
2018-10-12 15:51:26 +0200

    import my_swap

---
+++ my_swap/README.md
@@ -0,0 +1,9 @@
+The XOR operator can do a lot of things (very obscure sometimes)
+---------------
+
+### https://gist.github.com/b0f667c861faad8b6f7c5a2044ddfc94
+---------------
+
+
+Thanks [ReimuA](https://github.com/ReimuA) for contributing.
+For more tricks, look : https://graphics.stanford.edu/~seander/bithacks.html
--- my_swap/my_swap.c
+++ my_swap/my_swap.c
@@ -3,18 +3,18 @@
 
 // This should work on all primitive besides float.
 
-#define swap(x, y) do {					\
-    x = (typeof(x))((uintptr_t)x ^ (uintptr_t)y);	\
-    y = (typeof(x))((uintptr_t)y ^ (uintptr_t)x);	\
-    x = (typeof(x))((uintptr_t)x ^ (uintptr_t)y);	\
-  } while (0)
+#define my_swap(x, y) do {					\
+		x = (typeof(x))((uintptr_t)x ^ (uintptr_t)y);	\
+		y = (typeof(x))((uintptr_t)y ^ (uintptr_t)x);	\
+		x = (typeof(x))((uintptr_t)x ^ (uintptr_t)y);	\
+	} while (0)
 
 int main()
 {
-  char *foo = "foo";
-  char *bar = "bar";
+	char *foo = "foo";
+	char *bar = "bar";
 
-  printf("foo: %s, bar: %s\n", foo, bar);
-  swap(foo, bar);
-  printf("foo: %s, bar: %s\n", foo ,bar);
+	printf("foo: %s, bar: %s\n", foo, bar);
+	my_swap(foo, bar);
+	printf("foo: %s, bar: %s\n", foo ,bar);
 }
