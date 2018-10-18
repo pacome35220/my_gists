ac199ad3796b22d951df3fcf6f8399d9e1c4231a
ReimuA <thibault.dolley@epitech.eu>
2018-10-18 11:57:15 +0200

    .

--- deprecated/deprecated.c
+++ deprecated/deprecated.c
@@ -1,4 +1,35 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<string.h>
-void foo()__attribute__((deprecated));void foo(){write(1,"foo\n",strlen("foo\n"));}typedef int T __attribute__((deprecated));int g_var __attribute__((deprecated))=3231&312321;int main(){foo();T x __attribute__((unused));printf("%d\n",g_var);return 0;}
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+
+/*
+** Deprecating function.
+*/
+
+void foo() __attribute__((deprecated));
+
+void foo()
+{
+	write(1, "foo\n", strlen("foo\n"));
+}
+
+/*
+** Deprecate a type
+*/
+
+typedef int T __attribute__ ((deprecated));
+
+/*
+** Deprecate a variable
+*/
+
+int g_var __attribute__((deprecated)) = 3231 & 312321;
+
+int main()
+{
+	foo();	// Bro, this is deprecated, that's like... not cool.
+	T x __attribute__((unused)); // Another attribute ;)
+	printf("%d\n", g_var);
+	return 0;
+}
